3aff979e6a12f4612130a60ef2f7afd0d1da988a
Paul Osetinsky <paulosetinsky@Pauls-MacBook-Air.local>
2017-06-01 12:12:58 -0400

    messy code

--- ruby/video_quantizer.rb
+++ ruby/video_quantizer.rb
@@ -1,11 +1,16 @@
+require 'pry'
+require 'csv'
+
 class VideoQuantizer
+
+  MINUTE_SECONDS = 60.0
 
   attr_accessor :extract_scene_segments_command, :generate_altered_mp4s_command,
                 :onsets_hash, :perfect_beats, :path_to_video, :speed_multiples,
                 :speed_multiples_hash, :path_to_song, :path
 
   def initialize(onset_times, path_to_video, path_to_song, song_tempo=140)
-    main_beat = song_tempo.to_f/60.0
+    main_beat = song_tempo.to_f/MINUTE_SECONDS
 
     @extract_scene_segments_command = ""
     @generate_altered_mp4s_command = ""
@@ -13,9 +18,9 @@ class VideoQuantizer
     @path_to_video = path_to_video
     @path_to_song = path_to_song
     @perfect_beats = []
-    @path = `pwd`
+    @path = `pwd`.gsub("\n","")
 
-    num_perfect_beats = (video_duration_seconds.to_f / main_beat).round + 1
+    num_perfect_beats = (video_duration_seconds / main_beat).round + 2
     num_perfect_beats.times do |i|
       perfect_beats << (main_beat * i).round(2)
     end
@@ -38,7 +43,7 @@ class VideoQuantizer
 private
 
   def video_duration_seconds
-    `ffprobe -i #{path_to_video} -show_format -v quiet | sed -n 's/duration=//p'`
+    `ffprobe -i #{path_to_video} -show_format -v quiet | sed -n 's/duration=//p'`.to_f
   end
 
   def determine_quantization_diffs
@@ -96,7 +101,7 @@ private
 
       onsets_hash.delete_if do |original_onset_time,quantized_onset_time|
         original_onset_time != closest_original_onset_time &&
-        quantized_onset_time == dupe_quantized_onset_time
+          quantized_onset_time == dupe_quantized_onset_time
       end
     end
   end
@@ -140,15 +145,27 @@ private
 
   def write_output_commands
     last_onset = nil
+    last_idx = nil
+
     speed_multiples_hash.each_with_index do |onset_and_speed_multiple,idx|
       onset = onset_and_speed_multiple[0]
       mult  = onset_and_speed_multiple[1]
       extraction_start_time = extraction_start_time(last_onset, idx)
       duration = duration(onset, last_onset, idx)
+
       last_onset = onset
+      last_idx = idx
+
       write_extracted_scene_segment(extraction_start_time, duration, idx)
       write_generate_altered_mp4(mult, idx)
     end
+
+    current_idx = last_idx + 1
+    last_segment_start_time = extraction_start_time(last_onset, current_idx)
+    write_extracted_scene_segment(last_segment_start_time, video_duration_seconds - last_onset, current_idx)
+
+    # mult can be 1 here because we do not need to alter the speed for the last segment
+    write_generate_altered_mp4(1, last_idx + 1)
   end
 
   def write_quantized_onsets
@@ -206,7 +223,6 @@ private
 
   def write_generate_altered_mp4(mult, segment_idx)
     file_name = mezzanine_segment_file_name(segment_idx)
-    path = `pwd`
     output_segment_path = "#{path}/videos/output_segment_#{segment_idx}.mp4".gsub("\n","")
     segment_idx = stringify_segment_idx(segment_idx)
     command = "ffmpeg -i #{file_name} -filter:v \"setpts=#{(mult)}*PTS\" -an \ #{output_segment_path} && "
@@ -220,7 +236,15 @@ private
     extract_segments = extract_segments[0,extract_segments.rindex("&&")].strip
     alter_segments   = alter_segments[0,alter_segments.rindex("&&")].strip
 
-    `#{extract_segments} && #{alter_segments} && #{concatenate_segments} && #{add_song} && #{play_video}`
+    if `#{get_song_beats}`
+    `#{chop_song} &&
+     #{extract_segments} &&
+     #{alter_segments} &&
+     #{concatenate_segments} &&
+     #{add_song} &&
+     #{play_video}`
+    end
+
   end
 
   def concatenate_segments
@@ -229,11 +253,49 @@ private
   end
 
   def add_song
-    "ffmpeg -i videos/output.mp4 -i #{path_to_song} -c:v copy -map 0:v:0 -map 1:a:0 -shortest videos/output_with_music.mp4"
+    "ffmpeg -i videos/output.mp4 -i #{path_to_chopped_song} -c:v copy -map 0:v:0 -map 1:a:0 -shortest videos/output_with_music.mp4"
   end
 
   def play_video
     "ffplay -i -autoexit -showmode 0 videos/output_with_music.mp4"
+  end
+
+  def get_song_beats
+    "#{path}/librosa/examples/beat_tracker.py #{path_to_song} #{beat_output_csv_path}"
+  end
+
+  def beat_output_csv_path
+    path_to_song.gsub('.mp3', '_beats.csv')
+  end
+
+  def song_beat_first_onset
+    `touch #{beat_output_csv_path}`
+    csv = CSV.open(beat_output_csv_path)
+    beat_onsets = []
+    csv.each { |v| beat_onsets << v }
+    beat_onsets = beat_onsets.flatten.map(&:to_f)
+    durations = []
+
+    beat_onsets.each_with_index do |beat_onset, index|
+      if index > 0
+        durations << (beat_onsets[index] - beat_onsets[index-1]).round(3)
+      end
+    end
+
+    freq = durations.inject(Hash.new(0)) { |h,v| h[v] += 1; h }
+    mode = durations.max_by { |v| freq[v] }
+
+    first_beat_onset_index = durations.index(mode)
+
+    beat_onsets[first_beat_onset_index]
+  end
+
+  def chop_song
+    "ffmpeg -ss #{song_beat_first_onset} -i #{path_to_song} -acodec copy #{path_to_chopped_song}"
+  end
+
+  def path_to_chopped_song
+    path_to_song.gsub('.mp3', '_chopped.mp3')
   end
 
 end
